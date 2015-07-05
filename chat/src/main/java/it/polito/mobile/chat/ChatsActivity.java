4ce87d508526158013167cdb2ee2ed29a5e70d4e
speedymrk9 <marcogaido91@gmail.com>
2015-07-05 13:10:47 +0200

    Added parts to merge with the main app (everything is commented)
    Added parts to merge with the main app (everything is commented)

--- chat/src/main/java/it/polito/mobile/chat/ChatsActivity.java
+++ chat/src/main/java/it/polito/mobile/chat/ChatsActivity.java
@@ -17,23 +17,129 @@ public class ChatsActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chats);
+        //TODO added by marco, to be decommented to join with the app module
+        /*mNavigationDrawerFragment = (NavigationDrawerFragment) getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
+        mNavigationDrawerFragment.selectItem(getIntent().getIntExtra("position",1));
+
+        mNavigationDrawerFragment.setUp(R.id.navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout));
+        onSectionAttached(2);
+
+        setTitle(mTitle);
+        mNavigationDrawerFragment.setTitle(mTitle);*/
     }
+
+    //TODO added by marco, to be decommented to join with the app module
+    /*
+    public void onSectionAttached(int number) {
+		switch (number) {
+			case 1:
+				mTitle = getString(R.string.title_section1);
+				break;
+			case 2:
+				mTitle = getString(R.string.title_section2);
+				break;
+			case 3:
+				mTitle = getString(R.string.title_section3);
+				break;
+			case 4:
+				mTitle = getString(R.string.title_section4);
+				break;
+		}
+	}*/
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_chats, menu);
+        //TODO added by marco, to be decommented to join with the app module
+        //getMenuInflater().inflate(R.menu.global, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        boolean isGroup = id == R.id.action_group_message;
         Intent i = new Intent(ChatsActivity.this, SelectRecipientsActivity.class);
-        i.putExtra("isMultipleSelection", isGroup);
-        startActivityForResult(i, SELECT_RECIPIENTS);
+        switch(id){
+            case R.id.action_group_message:
+                i.putExtra("isMultipleSelection", true);
+                startActivityForResult(i, SELECT_RECIPIENTS);
+                return true;
+            case R.id.action_message:
+                i.putExtra("isMultipleSelection", false);
+                startActivityForResult(i, SELECT_RECIPIENTS);
+                return true;
+            //TODO added by marco, to be decommented to join with the app module
+            /*case R.id.action_logout:
+                showConfirmAlerter(0);
+                return true;
+
+            case R.id.action_delete:
+                showConfirmAlerter(1);
+                return true;*/
+
+        }
+
         return super.onOptionsItemSelected(item);
     }
+
+
+    //TODO added by marco, to be decommented to join with the app module
+    /*
+    public void showConfirmAlerter(int kind) {
+        AlertYesNo alert = new AlertYesNo();
+        Bundle info = new Bundle();
+        if (kind == 0)
+            info.putString("message", getResources().getString(R.string.logout_message));
+        else info.putString("message", getResources().getString(R.string.delete_user_message));
+
+        info.putString("title", getResources().getString(R.string.confirm));
+        info.putInt("kind", kind);
+        alert.setCommunicator(this);
+        alert.setArguments(info);
+        alert.show(getSupportFragmentManager(), "Confirm");
+
+    }
+    @Override
+    public void dialogResponse(int result, int kind) {
+        if (result == 1) {
+            switch (kind) {
+                case 0://logout
+                    getSharedPreferences("login_pref", MODE_PRIVATE).edit().clear().commit();
+                    Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+                    startActivity(i);
+                    finish();
+                    break;
+                case 1://delete account
+                    try {
+                        task = Manager.deleteStudent(((AppContext) getApplication()).getSession().getStudentLogged().getId(), new Manager.ResultProcessor<Integer>() {
+                            @Override
+                            public void process(Integer arg, Exception e) {
+                                task = null;
+                                if (e != null) {
+                                    Log.d(CompaniesFavouritesActivity.class.getSimpleName(), "Error deleteing user");
+                                    return;
+                                }
+                                getSharedPreferences("login_pref", MODE_PRIVATE).edit().clear().commit();
+                                Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+                                startActivity(i);
+                                finish();
+                            }
+
+                            @Override
+                            public void cancel() {
+                                task = null;
+                            }
+                        });
+                    } catch (DataFormatException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                default:
+                    return;
+            }
+        }
+    }
+    */
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
